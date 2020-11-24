c7d72df99e4ab55ae1800e972a879e5d9a36a7b8
dangnhon <dang.nhon12345@yahoo.com>
2020-11-24 15:43:13 -0800

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(strings)
+        @knowledge << strings
+    end 
+
+    def knowledge
+        @knowledge
+    end 
 
 end
